119949450157dad4ba868bd6a91a910ed9ba515e
Stensele <69506094+Stensele@users.noreply.github.com>
2020-09-16 18:41:07 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 alert('Hello world');
-let myName = 'Kai';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+
+let favoriteFood = "Kaaspressknoedel"
+document.write(favoriteFood);
